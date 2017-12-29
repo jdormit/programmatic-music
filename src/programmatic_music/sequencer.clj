e789169fb29331efaf9fced57d1cf80fc9b3af7a
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-12-29 09:33:23 -0500

    Implement make-sequencer macro

--- src/programmatic_music/sequencer.clj
+++ src/programmatic_music/sequencer.clj
@@ -1,5 +1,6 @@
 (ns programmatic-music.sequencer
-  (:require [overtone.live :refer [at apply-by]]))
+  (:require [overtone.live :refer [at apply-by]]
+            [clojure.core.match :refer [match]]))
 
 (defrecord Sequencer [step-index step-count step-mapping steps-enabled])
 
@@ -67,4 +68,58 @@
   [sequencer]
   "TODO: Implement me!")
 
-#_(defmacro make-sequencer)
+(defn make-sequencer-helper
+  "Helper function for the `make-sequencer` macro.
+
+  It processes the sequencer list representation row-by-row recursively."
+  [[current-form & rest-seq-list :as seq-list] row-count step-mapping]
+  (match current-form
+         (:or '. '!) (loop [[first & rest] seq-list
+                            idx 0
+                            steps-enabled #{}]
+                       (cond
+                         (nil? row-count) (throw
+                                           (RuntimeException.
+                                            "Sequencer must have at least one row"))
+                         (> idx row-count) (throw
+                                            (RuntimeException. "Row size does not match"))
+                         (nil? first) `(make-sequencer* ~row-count ~step-mapping ~steps-enabled)
+                         :else (match first
+                                      '. (recur rest (inc idx) steps-enabled)
+                                      '! (recur rest (inc idx) (conj steps-enabled idx))
+                                      x (throw (RuntimeException.
+                                                (str "Illegal sequencer expression. "
+                                                     "Expected . or !, found "
+                                                     x "."))))))
+         func-name (let [[row-step-map remaining-rows row-size]
+                         (loop [[first & rest :as remaining-rows] rest-seq-list
+                                row-size (or row-count 0)
+                                first-row? (nil? row-count)
+                                idx 0
+                                acc step-mapping]
+                           (if (and (not first-row?) (> idx row-size))
+                             (throw (RuntimeException. "Row size does not match"))
+                             (match first
+                                    '- (recur rest
+                                              (if first-row? (inc row-size) row-size)
+                                              first-row?
+                                              (inc idx)
+                                              acc)
+                                    'o (recur rest
+                                              (if first-row? (inc row-size) row-size)
+                                              first-row?
+                                              (inc idx)
+                                              (assoc acc idx func-name))
+                                    _ [acc remaining-rows row-size])))]
+                     (recur remaining-rows row-size row-step-map))))
+
+(defmacro make-sequencer
+  "Makes a sequencer from a visual representation.
+
+  Example usage: (make-sequencer (func1 - - - o
+                                  func2 - - - -
+                                  func3 o - o -
+                                        . ! ! !))
+  TODO: write extended description"
+  [seq-list]
+  (make-sequencer-helper seq-list nil {}))
